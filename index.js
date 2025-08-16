0109ab6f0f8c2576a374172e2151e94eb179d427
AzizbekUsmonjonov <azizbekusmonjonov464@gmail.com>
2025-08-16 12:08:23 +0300

    changed

---
+++ index.js
@@ -0,0 +1,59 @@
+// 1-masala ingani topish
+
+// let arr = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
+
+// function findNeedle(haystack) {
+//     const i = haystack.indexOf("needle");
+//     return `found the needle at position ${i}`;
+// }
+
+// console.log(findNeedle(arr));
+
+//====================================================================
+
+// 2-masala fake binary
+// function fakeBin(x) {
+//     return x
+//       .split("")
+//       .map(ch => (Number(ch) < 5 ? "0" : "1"))
+//       .join("");
+//   }
+
+//======================================================================
+
+//3-masala tosh, qaychi, qog'oz.
+
+// function rps(p1, p2) {
+//     if (p1 === p2) return "Draw!";
+  
+//     const wins = {
+//       rock: "scissors",
+//       scissors: "paper",
+//       paper: "rock"
+//     };
+  
+//     if (wins[p1] === p2) return "Player 1 won!";
+//     return "Player 2 won!";
+//   }
+  
+
+  
+//======================================================
+
+//4-masala svetafor
+
+// function updateLight(current) {
+//     const map = { green: "yellow", yellow: "red", red: "green" };
+//     return map[current];
+//   }
+
+//===============================================================
+
+//5-masala duble char
+
+// function doubleChar(str) {
+//     return str
+//       .split("")      
+//       .map(c => c + c) 
+//       .join("");     
+//   }
