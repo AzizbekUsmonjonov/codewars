6421f3a1edb7842d4212acda4658281297b424dd
AzizbekUsmonjonov <azizbekusmonjonov464@gmail.com>
2025-08-19 14:02:30 +0300

    changed

---
+++ app.js
@@ -0,0 +1,40 @@
+// 1-masala All Star Code Challenge #18
+
+// function strCount(str, letter){  
+//     let count = 0;
+//       for(let i of str){
+//         if(i === letter){
+//           count++;
+//         }
+//       }
+//       return count;
+//     }
+
+// ====================================================
+
+//2-masala Short Long Short
+
+// function solution(a, b) {
+//     if (a.length < b.length) {
+//       return a + b + a;
+//     } else {
+//       return b + a + b;
+//     }
+//   }
+
+//=================================================================
+
+//3-masala Find numbers which are divisible by given number 
+
+// function divisibleBy(numbers, divisor) {
+//     return numbers.filter(num => num % divisor === 0);
+//   }
+  
+
+//===================================================================
+
+// 4-masala Name Shuffler masalasi
+
+// function nameShuffler(name) {
+//   return name.split(" ").reverse().join(" ");
+// }
