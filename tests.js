8a96c2eb201f1a8d33318d2da8638283578454df
AzizbekUsmonjonov <azizbekusmonjonov464@gmail.com>
2025-09-22 22:37:30 +0300

    change

---
+++ tests.js
@@ -0,0 +1,151 @@
+//1-masala vowel count
+
+// function getCount(str) {
+//     let unliHarf = "aeiou";
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//       if (unliHarf.indexOf(str[i]) !== -1) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+
+//=========================================================================
+
+
+// returnig string masalasi
+
+// function greet(name){
+//     //your code here
+    
+//     return "Hello, " + name + " how are you doing today?";
+    
+//   }
+
+// ==================================================================================
+
+
+// basic mathematical operators
+
+
+// if (operation === '+') {
+//     function basicOp(operation, value1, value2){
+//         //Code
+//         if (operation === '+') {
+//         return value1 + value2
+//       } else if (operation === '-') {
+//         return value1 - value2
+//       } else if (operation === '*') {
+//         return value1 * value2
+//       } else if (operation === '/') {
+//         return value1 / value2
+//       } else {
+//         return NaN
+//       }
+//       }
+//============================================================================================
+
+
+// century From Year masalasi
+
+// function century(year) {
+//     // Finish this :
+//     let centnumber = year / 100;
+//     return Math.ceil(centnumber)
+//   }
+// =====================================================================================
+
+
+// Opassites Attark masalasi
+
+// function lovefunc(flower1, flower2) {
+//     if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+//       return true;
+//     } else if (flower1 % 2 !== 0 && flower2 % 2 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+// ================================================================================================
+
+// Keep Hydrated masalasi
+
+// function litres(time) {
+//     return Math.floor(time * 0.5);
+//   }
+
+// ==============================================================================================
+
+// Beginer Serias #1 School Paperwork
+
+// function paperwork(n, m) {
+//   if(n < 0 || m < 0){
+//     return 0;
+//   }else{
+//     return n * m;
+//   }
+// }
+
+//========================================================================================================
+
+
+//MakeUppercase masalasi 
+
+// function makeUpperCase(str) {
+//     return str.toUpperCase()
+//   }
+
+//==========================================================================================
+
+
+// Are you playing banjo
+
+
+// function areYouPlayingBanjo(name) {
+//     // Implement me
+//     if(name[0] === "R" || name[0] === "r"){
+//       return name + "plays banjo"
+//     }else{
+//       return name + "does not play banjo"
+//     }
+    
+//     return name;
+//   }
+
+// ===================================================================================================
+
+
+// Isograms masalasi
+
+// function isIsogram(str) {
+//     const s = str.toLowerCase().replace(/[^a-z]/g, '');
+//     return new Set(s).size === s.length;
+//   }
+  
+//=================================================================================================
+
+
+// Simple multiplication
+
+
+// function simpleMultiplication(number) {
+//     if(number % 2 === 0){
+//       return number * 8;
+//     }else{
+//       return number * 9;
+//     }
+//   }
+
+//============================================================================================
+
+
+// Beginner Series #2 Clock masalasi
+
+
+// function past(h, m, s){
+//     let millisekundlar = (h * 3600000) + (m * 60000) + (s * 1000)
+//     return millisekundlar
+//   }
